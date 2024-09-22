336497ed9c3a9a1eae361d9a9f53df7ef57c5794
Bruno Pereira <81038402+BrunoBzr@users.noreply.github.com>
2024-09-22 15:09:58 -0300

    Update clinica.c

--- clinica.c
+++ clinica.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 // \n  ||
 
-int qtd = 0;
-
 typedef struct{
 	char nomePaciente[50];
 	char racaPaciente[50];
@@ -13,13 +11,13 @@ typedef struct{
 	
 }Paciente;
 
-void cadastrarPaciente(FILE *arqPaciente, Paciente *paciente){
+void cadastrarPaciente(FILE *arqPaciente, Paciente *paciente, int *qtd){
 	
-	if(qtd == 0){
+	if(*qtd == 0){
 		paciente->id = 1;
+	}else{
+		paciente->id = *qtd + 1;
 	}
-	
-	paciente->id = qtd + 1;
 	
 	int op = 1;
 	do
@@ -82,7 +80,7 @@ void cadastrarPaciente(FILE *arqPaciente, Paciente *paciente){
 		
 	} while(op == 1);
 	
-	qtd++;
+	*qtd = *qtd + 1;
 	
 }//fim cadastrar paciente
 
@@ -115,13 +113,44 @@ void salvarPaciente(FILE *arqPaciente, Paciente *paciente){
 	fclose(arqPaciente);
 	
 	printf("Cliente cadastrado com sucesso!\n\n");
-	system("pause");
+}
+
+void mostrarPacientes(FILE *arqPaciente, Paciente *paciente, int *qtd){
+	arqPaciente = fopen("pacientes.txt", "r");
+	if(arqPaciente == NULL){
+		printf("Erro ao abrir o arquivo!\n");
+	}
+
+	if(*qtd == 0){
+		printf("Nenhum paciente cadastrado!\n");
+	}else{
+		printf("------------- LISTA DE PACIENTES ----------------\n\n");
+		int i;
+		for(i=0;i<*qtd;i++){
+			fscanf(arqPaciente, "%d\n", &paciente->id);
+			fgets(paciente->nomePaciente, 50, arqPaciente);
+			fgets(paciente->racaPaciente, 50, arqPaciente);
+			fscanf(arqPaciente, "%d\n", &paciente->idadePaciente);
+			fgets(paciente->nomeDono, 50, arqPaciente);
+			
+			printf("\n-------------------------\n\n");
+			printf("Id: %d\n",paciente->id);
+			printf("Nome do paciente: %s", paciente->nomePaciente);
+			printf("Raca do paciente: %s", paciente->racaPaciente);
+			printf("Idado do paciente: %d\n", paciente->idadePaciente);
+			printf("Nome do dono: %s", paciente->nomeDono);
+		}
+		printf("\n-------------------------\n");
+	}
+	
+	fclose(arqPaciente);
 }
 
 void main(){
 	
 	FILE *arqPaciente;
 	Paciente paciente;
+	int qtd = 0;
 	int opWhile = 1;
 	
 	while(opWhile != 0){
@@ -138,8 +167,14 @@ void main(){
 		case 1:
 			system("cls");
 			fflush(stdin);
-			cadastrarPaciente(arqPaciente, &paciente);
+			cadastrarPaciente(arqPaciente, &paciente, &qtd);
 			salvarPaciente(arqPaciente, &paciente);
+			system("pause");
+			break;
+		case 3:
+			system("cls");
+			fflush(stdin);
+			mostrarPacientes(arqPaciente, &paciente, &qtd);
 			system("pause");
 			break;
 		case 0:
